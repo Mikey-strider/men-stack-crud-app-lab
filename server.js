52306b2859c50fc9e6d46545668720c04af2370f
Dromen2001 <dromen2001@gmail.com>
2024-06-04 09:01:44 -0700

    Finished CRUD functionality  Just need to add styling

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+// ===================Import Models====================
+
+
 const dotenv = require("dotenv"); // require package
 dotenv.config(); // Loads the environment variables from .env file
 const express = require("express");
 const mongoose = require("mongoose"); // require package
+const methodOverride = require("method-override");
 const morgan = require("morgan");
+const path = require('path')
 
-// ===================Import Models====================
+
+
 const HorrorModel = require('./models/horrorMovies');
 
 const app = express();
@@ -23,10 +29,34 @@ app.listen(3000, () => {
 // =================MiddleWare==================
 
 app.use(express.urlencoded({extended: false}));
+app.use(methodOverride("_method"))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use(morgan('dev'));
 
 //==================Routes======================
+
+
+// The Edit Routes================================
+
+app.get('/movies/:moviesId/update', async (req, res) => {
+  const horrorDoc = await HorrorModel.findByIdAndUpdate(req.params.moviesId)
+  res.render('movies/update.ejs', {horrorDoc: horrorDoc})
+})
+
+app.put('/movies/:movieId', async (req, res) => {
+  const updateHorror = await HorrorModel.findByIdAndUpdate(req.params.movieId, req.body, {new: true});
+  res.redirect(`/movies/${req.params.movieId}`);
+});
+
+// The Delete Routes==============================
+
+
+app.delete('/movies/:movieId', async (req, res) => {
+  const deletedMovie = await HorrorModel.findByIdAndDelete(req.params.fruitId);
+  res.redirect('/movies');
+})
+
 
 // The Create Routes==============================
 app.get('/movies/new', (req, res) => {
@@ -41,16 +71,24 @@ app.post('/movies', async (req, res) => {
 })
 
 
+// The Show Routes================================
+
+app.get('/movies/:movieId', async (req, res) => {
+  const horrorDoc = await HorrorModel.findById(req.params.movieId);
+  res.render('movies/show.ejs', {horrorDoc: horrorDoc});
+})
+
 
 // The Read Routes================================
 
 // The Index Routes===============================
 
-// The Show Routes================================
+app.get('/movies', async (req, res) => {
+  const horrorDocs = await HorrorModel.find({});
+  res.render('movies/index.ejs', {horrorDocs: horrorDocs});
+})
 
-// The Edit Routes================================
 
-// The Delete Routes==============================
 
 
 
